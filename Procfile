2fefae593636dd963e7f2fb1e28ae7c782b2c8b0
saketh-crystalwg <saketh@crystalwg.com>
2024-05-15 22:07:27 +0530

    first push

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python betfoxx_txn_alerts.py
---
+++ betfoxx_txn_alerts.py
@@ -0,0 +1,137 @@
+import json
+import pandas as pd
+import  requests
+from requests.auth import HTTPBasicAuth
+from sqlalchemy import create_engine
+import smtplib,ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from email import encoders
+import datetime as dt
+from datetime import datetime, timedelta
+from openpyxl.styles import Alignment
+
+
+def send_mail(send_from, send_to, subject, text, server, port, username='', password='', filename=None):
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = ', '.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text))
+
+    if filename is not None:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(filename, "rb").read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename={filename}')
+        msg.attach(part)
+
+    smtp = smtplib.SMTP_SSL(server, port)
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+start_time = datetime.utcnow() - timedelta(hours=1)
+
+end_time = datetime.utcnow()
+
+# Extract only the date and hour from the current system datetime
+start_datetime = start_time.strftime('%Y-%m-%dT%H:00:00.000Z')
+
+end_datetime = end_time.strftime('%Y-%m-%dT%H:00:00.000Z')
+
+txn_url = 'https://adminwebapi.iqsoftllc.com/api/Main/ApiRequest?TimeZone=4&LanguageId=en'
+
+txn_data = {"Controller":"PaymentSystem",
+            "Method":"GetPaymentRequestsPaging",
+            "RequestObject":{
+                "Controller":"PaymentSystem",
+                "Method":"GetPaymentRequestsPaging",
+                "SkipCount":0,
+                "TakeCount":1000,
+                "OrderBy":None,
+                "FieldNameToOrderBy":"",
+                "Type":2,
+                "HasNote":False,
+                "FromDate":start_datetime,"ToDate":end_datetime},
+            "UserId":"1780","ApiKey":"betfoxx_api_key"}
+
+txn_response = requests.post(txn_url, json=txn_data)
+
+txn_response_data = txn_response.json()
+
+txn_entities = txn_response_data['ResponseObject']['PaymentRequests']['Entities']
+
+txns = pd.DataFrame(txn_entities)
+
+end_datetime_1 = end_time.strftime('%Y-%m-%d_T%H:00')
+
+if txns is not None and txns.shape[0] > 0:
+    txns['Status'] = ['Approved' if x == 8 \
+                      else 'ApprovedManually' if x == 12 \
+                      else 'Cancelled' if x == 2 \
+                      else 'CancelPending' if x == 14 \
+                      else 'Confirmed' if x == 7 \
+                      else 'Declined' if x == 6 \
+                      else 'Deleted' if x == 11 \
+                      else 'Expired' if x == 13 \
+                      else 'Failed' if x == 9 \
+                      else 'Frozen' if x == 4 \
+                      else 'InProcess' if x == 3 \
+                      else 'Pay Pending' if x == 10 \
+                      else 'Pending' if x == 1 \
+                      else 'Splitted' if x == 15 \
+                      else 'Waiting For KYC' if x == 5 \
+                      else 'NA' for x in txns['State']]
+
+    filtered_txns = txns[(txns['Status'] != 'Approved') & (txns['Status'] != 'ApprovedManually')][['UserName', 'FirstName', 'LastName', 'Email', 'CreationTime', 'PaymentSystemId', 'Status', 'CurrencyId', 'Amount']]
+    filtered_txns['Payment_Method'] = ['InternationalPSP' if x == 326 \
+                                       else 'NOWPay' if x == 147 \
+                                       else 'XcoinsPayCard' if x == 324 \
+                                       else 'XcoinsPayCrypto' if x == 323 \
+                                       else 'Others' for x in filtered_txns['PaymentSystemId']]
+
+    filename = f'Betfoxx_Transaction_Alerts_{end_datetime_1}.xlsx'
+
+    sub = f'Betfoxx_Transaction_Details_{end_datetime_1}'
+
+    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+        filtered_txns.reset_index(drop=True).to_excel(writer, sheet_name="Unsuccessful_Txns", index=False)
+
+    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
+        workbook = writer.book
+        worksheet1 = writer.sheets['Unsuccessful_Txns']
+
+        for column in worksheet1.columns:
+            max_length = 0
+            column_name = column[0].column_letter
+            for cell in column:
+                if cell.value:
+                    max_length = max(max_length, len(str(cell.value)))
+            adjusted_width = (max_length + 2) * 1.2
+            worksheet1.column_dimensions[column_name].width = adjusted_width
+
+        for column in worksheet1.iter_cols(min_col=1, max_col=len(filtered_txns.columns)):
+            for cell in column:
+                cell.alignment = Alignment(horizontal='center')
+
+    subject = sub
+    body = f"Hi,\n\n Attached contains the details of unsuccessfull transactions  during the  hour of  {end_datetime_1} for Betfoxx \n\nThanks,\nSaketh"
+    sender = "sakethg250@gmail.com"
+    recipients = ["saketh@crystalwg.com","sebastian@crystalwg.com","isaac@crystalwg.com"]
+    password = "xjyb jsdl buri ylqr"
+
+    send_mail(sender, recipients, subject, body, "smtp.gmail.com", 465, sender, password, filename)
+
+else:
+    # Send an email indicating there are no transactions
+    subject = f'Betfoxx_Transaction_Details_{end_datetime_1}'
+    body = "Hi,\n\nNo unsuccessful transactions were found during the specified period.\n\nThanks,\nSaketh"
+    sender = "sakethg250@gmail.com"
+    recipients = ["saketh@crystalwg.com","sebastian@crystalwg.com","isaac@crystalwg.com"]
+    password = "xjyb jsdl buri ylqr"
+
+    send_mail(sender, recipients, subject, body, "smtp.gmail.com", 465, sender, password)
