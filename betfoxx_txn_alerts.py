a5d1a7a8d19c7661327abb2e33599c20b87cb1a7
saketh-crystalwg <saketh@crystalwg.com>
2024-06-22 18:48:15 +0530

    else addition

--- betfoxx_txn_alerts.py
+++ betfoxx_txn_alerts.py
@@ -154,3 +154,11 @@ if txns is not None and txns.shape[0] > 0:
     password = "xjyb jsdl buri ylqr"
 
     send_mail(sender, recipients, subject, body, "smtp.gmail.com", 465, sender, password, filename)
+else:
+    subject = f'Betfoxx_Transaction_Details_{end_datetime_1}'
+    body = "Hi,\n\nNo failed transactions during the specified period.\n\nThanks,\nSaketh"
+    sender = "sakethg250@gmail.com"
+    recipients = ["saketh@crystalwg.com","sebastian@crystalwg.com","SANDRA@CRYSTALWG.COM","ron@crystalwg.com","camila@crystalwg.com","celeste@crystalwg.com","cristina@crystalwg.com","lina.betcoco@gmail.com","erika@crystalwg.com"]
+    password = "xjyb jsdl buri ylqr"
+
+    send_mail(sender, recipients, subject, body, "smtp.gmail.com", 465, sender, password)
